e9314e758b21d888a8db4f4a2168c399b5f3ad27
5oko1 <severov.egor@yandex.ru>
2022-02-17 22:02:07 +0300

    fix book displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,27 @@
+import datetime
+
 from django.shortcuts import render
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    all_books = Book.objects.all()
+    context = {'books': all_books}
+    return render(request, template, context)
+
+
+def pub_date_view(request, date):
+    template = 'books/publish_catalog.html'
+    date_params = date.split('-')
+    date_params = [int(d) for d in date_params]
+    user_date = datetime.date(*date_params)
+    published_books = Book.objects.filter(pub_date=user_date)
+
+    prev_date = user_date - datetime.timedelta(days=1)
+    future_date = user_date + datetime.timedelta(days=1)
+    context = {'books': published_books,
+               'previous_date': prev_date.strftime('%Y-%m-%d'),
+               'future_date': future_date.strftime('%Y-%m-%d')}
     return render(request, template, context)
